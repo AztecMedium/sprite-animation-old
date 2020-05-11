8b82e4703cb952c88a5ce53f5dc0dd6dfe5f173d
AztecMedium <59755939+AztecMedium@users.noreply.github.com>
2020-05-11 23:13:16 +0600

    Финальная версия работы (или предфинальная)

--- src/Main.java
+++ src/Main.java
@@ -1,23 +1,21 @@
 import javafx.animation.Animation;
+import javafx.animation.PathTransition;
 import javafx.animation.Transition;
 import javafx.application.Application;
 import javafx.geometry.Rectangle2D;
 import javafx.scene.Group;
 import javafx.scene.Scene;
 import javafx.scene.image.*;
-import javafx.scene.layout.StackPane;
+import javafx.scene.shape.Polyline;
 import javafx.stage.Stage;
 import javafx.util.Duration;
-import javafx.scene.layout.Pane;
-import javafx.scene.canvas.Canvas;
-import javafx.scene.shape.Path;
-import javafx.scene.shape.MoveTo;
-import javafx.scene.shape.LineTo;
+import java.lang.Math;
 
 public class Main extends Application{
     private static final int TOLERANCE_THRESHOLD = 0xff;
     private final Image IMAGE = new Image(getClass().getResourceAsStream("Snow.gif"));
     private final Image STATICIMAGE = new Image(getClass().getResourceAsStream("Tree.gif"));
+    private final Image BACKGROUNDIMAGE = new Image(getClass().getResourceAsStream("background.jpg"));
     private static final int COLUMNS = 4;
     private static final int COUNT = 4;
     private static final int OFFSET_X = 0;
@@ -55,39 +53,133 @@ public class Main extends Application{
     }
     @Override
     public void start (Stage primaryStage){
-        Pane root = new Pane();
-
-        StackPane holder = new StackPane();
-        Canvas canvas = new Canvas(500,  500);
-
-        holder.getChildren().add(canvas);
-        root.getChildren().add(holder);
-
-        holder.setStyle("-fx-background-color: darkcyan");
         final Image DynamicResampledImage = makeTransparent(IMAGE);
         final ImageView DynamicResampledImageView = new ImageView(DynamicResampledImage);
         final Image StaticResampledImage = makeTransparent(STATICIMAGE);
         final ImageView StaticResampledImageView = new ImageView(StaticResampledImage);
-        StaticResampledImageView.setViewport(new Rectangle2D(-100,0,StaticWidth,StaticHeight));
+        final Image BackGroundImage = makeTransparent(BACKGROUNDIMAGE);
+        final ImageView BackGroundImageView = new ImageView((BackGroundImage));
+        final ImageView St1 = new ImageView(StaticResampledImage);
+        final ImageView D1V = new ImageView(DynamicResampledImage);
+        final ImageView D2V = new ImageView(DynamicResampledImage);
+        final ImageView D3V = new ImageView(DynamicResampledImage);
+        final ImageView D4V = new ImageView(DynamicResampledImage);
+        St1.setViewport(new Rectangle2D(-250,-250,StaticWidth,StaticHeight));
+        StaticResampledImageView.setViewport(new Rectangle2D(0,-200,StaticWidth,StaticHeight));
         DynamicResampledImageView.setViewport(new Rectangle2D(OFFSET_X, OFFSET_Y, WIDTH, HEIGHT));
-        Path path = new Path();
-        path.getElements().add(new MoveTo(100, 100));
-        path.getElements().add(new LineTo(0, 0));
+        BackGroundImageView.setViewport((new Rectangle2D(0,0,500,500)));
+        D1V.setViewport(new Rectangle2D(OFFSET_X, OFFSET_Y, WIDTH, HEIGHT));
+        Polyline polyline = new Polyline();
+        polyline.getPoints().addAll((new Double[]{
+                Math.random()*500, -50.0,
+                Math.random()*500, 530.0}));
+        PathTransition transition = new PathTransition();
+        transition.setNode(DynamicResampledImageView);
+        transition.setDuration(Duration.seconds(2));
+        transition.setPath(polyline);
         final Animation _static = new StaticObject(
                 StaticResampledImageView, StaticWidth,StaticHeight
         );
+        final Animation st1 = new StaticObject(
+                St1, StaticWidth,StaticHeight
+        );
         final Animation animation = new SpriteAnimation(
                 DynamicResampledImageView,
-                Duration.millis(500),
+                Duration.millis(300),
                 COUNT, COLUMNS,
                 OFFSET_X, OFFSET_Y,
                 WIDTH, HEIGHT
         );
         animation.setCycleCount(Animation.INDEFINITE);
+        transition.setCycleCount(PathTransition.INDEFINITE);
+        transition.play();
+        st1.play();
         _static.play();
         animation.play();
 
-        primaryStage.setScene(new Scene(new Group(root,StaticResampledImageView,DynamicResampledImageView),500,500));
+        Polyline polyline1 = new Polyline();
+        polyline1.getPoints().addAll((new Double[]{
+                Math.random()*500, -100.0,
+                Math.random()*500, 530.0}));
+        PathTransition tr1 = new PathTransition();
+        tr1.setNode(D1V);
+        tr1.setDuration(Duration.seconds(2));
+        tr1.setPath(polyline1);
+
+        final Animation an1 = new SpriteAnimation(
+                D1V,
+                Duration.millis(250),
+                COUNT, COLUMNS,
+                OFFSET_X, OFFSET_Y,
+                WIDTH, HEIGHT
+        );
+
+        an1.setCycleCount(Animation.INDEFINITE);
+        tr1.setCycleCount(PathTransition.INDEFINITE);
+        tr1.play();
+
+        an1.play();
+        D2V.setViewport(new Rectangle2D(OFFSET_X, OFFSET_Y, WIDTH, HEIGHT));
+        Polyline polyline2 = new Polyline();
+        polyline2.getPoints().addAll((new Double[]{
+                Math.random()*500, -130.0,
+                Math.random()*500, 530.0}));
+        PathTransition tr2 = new PathTransition();
+        tr2.setNode(D2V);
+        tr2.setDuration(Duration.seconds(4.0));
+        tr2.setPath(polyline2);
+        final Animation an2 = new SpriteAnimation(
+                D2V,
+                Duration.millis(200),
+                COUNT, COLUMNS,
+                OFFSET_X, OFFSET_Y,
+                WIDTH, HEIGHT
+        );
+        an2.setCycleCount(Animation.INDEFINITE);
+        tr2.setCycleCount(PathTransition.INDEFINITE);
+        tr2.play();
+        an2.play();
+        D3V.setViewport(new Rectangle2D(OFFSET_X, OFFSET_Y, WIDTH, HEIGHT));
+        Polyline polyline3 = new Polyline();
+        polyline3.getPoints().addAll((new Double[]{
+                Math.random()*500, -200.0,
+                Math.random()*500, 530.0}));
+        PathTransition tr3 = new PathTransition();
+        tr3.setNode(D3V);
+        tr3.setDuration(Duration.seconds(3.2));
+        tr3.setPath(polyline3);
+        final Animation an3 = new SpriteAnimation(
+                D3V,
+                Duration.millis(190),
+                COUNT, COLUMNS,
+                OFFSET_X, OFFSET_Y,
+                WIDTH, HEIGHT
+        );
+        an3.setCycleCount(Animation.INDEFINITE);
+        tr3.setCycleCount(PathTransition.INDEFINITE);
+        tr3.play();
+        an3.play();
+        D4V.setViewport(new Rectangle2D(OFFSET_X, OFFSET_Y, WIDTH, HEIGHT));
+        Polyline polyline4 = new Polyline();
+        polyline4.getPoints().addAll((new Double[]{
+                Math.random()*500, -300.0,
+                Math.random()*500, 530.0}));
+        PathTransition tr4 = new PathTransition();
+        tr4.setNode(D4V);
+        tr4.setDuration(Duration.seconds(4.3));
+        tr4.setPath(polyline4);
+        final Animation an4 = new SpriteAnimation(
+                D4V,
+                Duration.millis(274),
+                COUNT, COLUMNS,
+                OFFSET_X, OFFSET_Y,
+                WIDTH, HEIGHT
+        );
+        an4.setCycleCount(Animation.INDEFINITE);
+        tr4.setCycleCount(PathTransition.INDEFINITE);
+        tr4.play();
+        an4.play();
+        primaryStage.setScene(new Scene(new Group(BackGroundImageView,StaticResampledImageView,DynamicResampledImageView,D1V,St1,D2V,D3V,D4V),500,500));
         primaryStage.show();
     }
     public static void main (String[] args){
